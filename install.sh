71d93dc3d95db5731d043f1efe030a4fc29b9d65
Hengne <Hengne.Li@CERN.CH>
2014-06-30 13:19:52 +0200

    fix install scripts.

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 scramv1 project CMSSW CMSSW_5_3_9_patch3
 cd CMSSW_5_3_9_patch3/src
 eval `scramv1 runtime -sh`
-git remote add origin https://github.com/VBF-HZZ/UFHiggsPattuplizer8TeV.git
-git fetch origin
-git checkout testProd
+git clone https://github.com/VBF-HZZ/UFHiggsPattuplizer8TeV.git
+cd UFHiggsPattuplizer8TeV
+git checkout -b testProd origin/testProd
+cd ../
 ln -s UFHiggsPattuplizer8TeV/Utilities/* ./
 #make sure you have 'export CVSROOT=":ext:<username>@lxplus5.cern.ch:/afs/cern.ch/user/c/cvscmssw/public/CMSSW"; export CVS_RSH=/usr/bin/ssh' in your .bash_profile
 python installPackages.py
