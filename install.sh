adafc61fc4b5be1b655214d41ed5bab5d47a78ad
Hengne <Hengne.Li@CERN.CH>
2014-07-01 11:04:55 +0200

    update install.sh not to use CVS anymore for installation packages

--- install.sh
+++ install.sh
@@ -8,6 +8,5 @@ cd UFHiggsPattuplizer8TeV
 git checkout -b testProd origin/testProd
 cd ../
 ln -s UFHiggsPattuplizer8TeV/Utilities/* ./
-#make sure you have 'export CVSROOT=":ext:<username>@lxplus5.cern.ch:/afs/cern.ch/user/c/cvscmssw/public/CMSSW"; export CVS_RSH=/usr/bin/ssh' in your .bash_profile
-python installPackages.py
+tar xvzf requiredPackages.tgz
 scram b -j12
